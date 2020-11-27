3bae21e572c5465b3c83f99a9b4c7d3e0e1e1e34
Rui Ramos <47954852+RuiRaminhos47@users.noreply.github.com>
2020-11-27 19:42:35 +0000

    Add files via upload

--- mazeRunner.py
+++ mazeRunner.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 import cv2
+import random
 # Colors format of opencv: BGR
 
 cap = cv2.VideoCapture(0)
@@ -275,11 +276,62 @@ gameField = buildMaze(choosenMaze);
 
 # Let's start playing!
 clearConsole()
-while actualPosition_X != finalPosition_X or actualPosition_Y != finalPosition_Y:
+while(True):
+
+    if(actualPosition_X == finalPosition_X and actualPosition_Y == finalPosition_Y):
+        break
+
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # flip
+    frame = cv2.flip(frame, 180) 
+
+    # rectangle LEFT
+    rec1_pt1 = (20, 166)
+    rec1_pt2 = (206, 312)
+    rec1 = cv2.rectangle(frame, rec1_pt1, rec1_pt2, (180,105,255), 3)
+
+    # rectangle RIGHT
+    rec2_pt1 = (432, 166)
+    rec2_pt2 = (622, 312)
+    rec2 = cv2.rectangle(frame, rec2_pt1, rec2_pt2, (0,0,250), 3)
+
+    # rectangle UP
+    rec3_pt1 = (226, 10)
+    rec3_pt2 = (412, 156)
+    rec3 = cv2.rectangle(frame, rec3_pt1, rec3_pt2, (0,128,0), 3)
+
+    # rectangle DOWN
+    rec4_pt1 = (226, 324)
+    rec4_pt2 = (412, 470)
+    rec4 = cv2.rectangle(frame, rec4_pt1, rec4_pt2, (255,144,30), 3)
+
+    # center circle
+    center = (320, 240)
+    radius = 60
+    circle = cv2.circle(frame, center, radius, (255,255,255), thickness=3, lineType=8, shift=0) 
+
+    # Display the resulting frame
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
     clearConsole()
     printGameField(gameField, actualPosition_X, actualPosition_Y)
             
-    chooseDirection = input()
+    #chooseDirection = input()
+
+    # Test Input Random
+    randomNumberDirection = random.randint(1,4)
+    if randomNumberDirection==1:
+        chooseDirection = "A"
+    if randomNumberDirection==2:
+        chooseDirection = "S"
+    if randomNumberDirection==3:
+        chooseDirection = "D"
+    if randomNumberDirection==4:
+        chooseDirection = "W"
 
     if chooseDirection=="A":
         if possiblePlay(gameField, actualPosition_X, actualPosition_Y, "A"): 
